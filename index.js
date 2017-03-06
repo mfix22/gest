e66e86b71b9daad7eb849473f39a6117fde0bbc1
Mike Fix <mrfix84@gmail.com>
2017-03-06 17:01:35 -0600

    Lint. Refactor REPL

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 #! /usr/bin/env node
 const { sendQuery, readFile, checkPath, REPL } = require('./src/util')
 
-const [node, file, ...args] = process.argv
+const args = process.argv.slice(2)
 
 if (args.length && args.length % 2 === 1) {
   const queryOrFile = args.pop()
 
-  return checkPath(`${__dirname}/${queryOrFile}`)
-          .then(readFile)
-          .catch(() => queryOrFile)
-          .then(sendQuery(args))
-          .then(console.log)
-          .catch(console.log)
+  checkPath(`${__dirname}/${queryOrFile}`)
+    .then(readFile)
+    .catch(() => queryOrFile)
+    .then(sendQuery(args))
+    .then(console.log)
+    .catch(console.log)
+
+  process.exit()
 }
 
 // REPL
--- src/util.js
+++ src/util.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const chalk = require('chalk')
 const { graphql } = require('graphql')
 const { graphql: config } = require(`${process.cwd()}/package.json`)
-const { baseURL, timeout } = config
+const { baseURL } = config
 
 const encode = query => `"${query.replace(/\s/ig, '').replace(/"/ig, `\\"`).toString()}"`
 
@@ -22,7 +22,7 @@ function REPL (args) {
       sendQuery(args)(query)
         .then(console.log)
         .catch(console.log)
-      prompt(args)
+        .then(() => prompt(args))
     })
   }
 
@@ -40,7 +40,7 @@ const pullHeaders = (args) =>
 
 function sendQuery (args) {
   return function (query) {
-    console.log(`${query}\n`);
+    console.log(`${query}\n`)
     if (baseURL) {
       const instance = axios.create({
         baseURL,
@@ -59,22 +59,20 @@ function sendQuery (args) {
   }
 }
 
-
 const colorResponse = (res) =>
   JSON.stringify(res, null, 2)
     .replace(/"errors":/ig, `${chalk.red.bold('"errors"')}:`)
-    .replace(/"data":/ig, `${chalk.green.bold('"data"')}:`)
+    .replace(/"data":/ig, `${chalk.green.bold('"data"')}:`) + '\n'
 
 const readFile = (path) =>
   new Promise((resolve, reject) =>
-    fs.readFile(path, 'utf8', (err,data) =>
+    fs.readFile(path, 'utf8', (err, data) =>
       err ? reject(err) : resolve(data)))
 
 const checkPath = (path) =>
   new Promise((resolve, reject) =>
     fs.stat(path, (err, stats) =>
       err ? reject(err) : resolve(path)))
-
 
 module.exports = {
   encode,
