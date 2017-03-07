9806bb5b2212602d8ee777b3bcac01964e3d8f2e
Mike Fix <mrfix84@gmail.com>
2017-03-07 14:56:51 -0600

    Refactor domain checking and error messages

--- src/util.js
+++ src/util.js
@@ -30,6 +30,7 @@ function REPL (args) {
 }
 
 const pullHeaders = ({ header }) => {
+  if (!header) return {}
   if (typeof header === 'string') {
     const [key, value] = header
     return { [key]: value }
@@ -41,31 +42,35 @@ const pullHeaders = ({ header }) => {
   }, {})
 }
 
+const testURL = (url) => /(^|\s)((https?:\/\/)[\w-]+(\.[a-z-]+)+\.?(:\d+)?(\/\S*)?)/gi.test(url)
+
 function sendQuery (flags) {
   return function (query) {
     console.log(`${query}\n`)
     if (config && config.baseURL) {
+      if (!testURL(config.baseURL)) throw new Error('Your `baseUrl` must be a domain with "http(s)"')
+
       const instance = axios.create({
-        baseURL: config.baseURL,
         timeout: config.timeout || 5000,
         headers: pullHeaders(flags)
       })
-      return instance.post('/', encode(query))
+
+      return instance.post(config.baseURL, encode(query))
                      .then(res => res.data)
                      .then(colorResponse)
-                     .catch(console.log)
     }
     const schema = require(path.join(process.cwd(), (config && config.schema) || 'schema.js'))
     return graphql(schema, query)
             .then(colorResponse)
-            .catch(console.log)
   }
 }
 
 const colorResponse = (res) =>
-  JSON.stringify(res, null, 2)
-    .replace(/"errors":/ig, `${chalk.red.bold('"errors"')}:`)
-    .replace(/"data":/ig, `${chalk.green.bold('"data"')}:`) + '\n'
+  `${JSON.stringify(res, null, 2)
+    .replace(/errors/i, chalk.red.bold('$&'))
+    .replace(/data/i, chalk.green.bold('$&'))
+    .replace(/\\"/ig, `"`)
+    .replace(/Did you mean ".+?"/ig, chalk.yellow.bold('$&'))}\n`
 
 const readFile = (path) =>
   new Promise((resolve, reject) =>
