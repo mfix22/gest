5eb842d09ae6f0fb05d9a3d30c47c6eb188866fe
Mike Fix <mrfix84@gmail.com>
2017-03-07 21:22:50 -0600

    Fix finding schema bug

--- gest.js
+++ gest.js
@@ -1,11 +1,9 @@
 #! /usr/bin/env node
-const args = require('args')
+const args = require('./args') // TODO
 const path = require('path')
 const { graphql: config } = require(path.join(process.cwd(), 'package.json'))
 const { sendQuery, readFile, checkPath, REPL } = require('./src/api')
 const { pullHeaders, colorResponse } = require('./src/util')
-
-const schema = require(path.join(process.cwd(), (config && config.schema) || 'schema.js'))
 
 args
   .option('header', 'HTTP request header')
@@ -13,10 +11,19 @@ args
 
 const flags = args.parse(process.argv)
 
-const options = Object.assign({}, config, pullHeaders(flags), { baseURL: flags.baseUrl })
+if (!flags.help && !flags.version) {
+  let schema
+  try {
+    schema = require(path.join(process.cwd(), (config && config.schema) || 'schema.js'))
+  } catch (e) {
+    console.log('\nSchema not found. Trying running `gest` with your `schema.js` in the current working directory.\n')
+    process.exit()
+  }
 
-if (args.sub && args.sub.length) {
-  checkPath(path.join(__dirname, args.sub[0]))
+  const options = Object.assign({}, config, pullHeaders(flags), { baseURL: flags.baseUrl })
+
+  if (args.sub && args.sub.length) {
+    checkPath(path.join(__dirname, args.sub[0]))
     .then(readFile)
     .catch(() => args.sub[0])
     .then(sendQuery(schema, options))
@@ -24,7 +31,8 @@ if (args.sub && args.sub.length) {
     .then(console.log)
     .catch(console.log)
     .then(() => process.exit())
-} else {
-  // REPL
-  REPL(schema, options)
+  } else {
+    // REPL
+    REPL(schema, options)
+  }
 }
