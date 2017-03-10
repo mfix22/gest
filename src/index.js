b1b95a96e7c7b8bbc4c97a43512e8130c3933c49
Mike Fix <mrfix84@gmail.com>
2017-03-09 19:53:38 -0800

    Add config default

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { graphql } = require('graphql')
 
 const { correctURL, encode, DEFAULT_CONFIG } = require('./util')
 
-function Gest (schema, config) {
+function Gest (schema, config = {}) {
   const { baseURL, headers, timeout } = Object.assign(DEFAULT_CONFIG, config)
   return function (query) {
     if (baseURL) {
@@ -13,13 +13,13 @@ function Gest (schema, config) {
       })
 
       const corrected = correctURL(baseURL)
-      console.log(`${query} -> ${corrected}`)
+      if (config.verbose) console.log(`${query} -> ${corrected}`)
 
       return instance.post(corrected, encode(query))
                      .then(res => res.data)
     }
 
-    console.log(query)
+    if (config.verbose) console.log(query)
     return graphql(schema, query)
   }
 }
