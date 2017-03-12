b288205a987f48da432923377c3beeff85a0225c
Mike Fix <mrfix84@gmail.com>
2017-03-11 18:43:49 -0800

    Works

--- bin/gest.js
+++ bin/gest.js
@@ -2,16 +2,18 @@
 const args = require('args')
 const path = require('path')
 const { printSchema } = require('graphql')
+const chalk = require('chalk')
 
 const gest = require('../src/index')
 const REPL = require('../src/REPL')
-const { readFile, checkPath, flagsToOptions, colorResponse, errorMessage } = require('../src/util')
+const { readFile, checkPath, flagsToOptions, colorResponse, errorMessage, findFiles } = require('../src/util')
 
 args
   .option(['S', 'schema'], 'Path to your GraphQL schema')
   .option(['I', 'inspect'], 'Print your GraphQL schema options')
   .option(['H', 'header'], 'HTTP request header')
   .option(['B', 'baseUrl'], 'Base URL for sending HTTP requests')
+  .option('all', 'Run all *.query files')
 
 const flags = args.parse(process.argv)
 try {
@@ -30,17 +32,39 @@ try {
       process.exit()
     }
 
-    if (args.sub && args.sub.length) {
-      checkPath(path.join(process.cwd(), args.sub[0]))
-      .then(readFile)
-      .catch(() => args.sub[0])
-      .then(gest(schema, options))
-      .then(colorResponse)
-      .then(console.log)
-      .catch(console.log)
+    if (flags.all) {
+      findFiles()
+        .then(values =>
+          values.map(v => {
+            console.log(`${chalk.black.bgYellow(' RUNS ')} ${chalk.dim(v.replace(process.cwd(), '.'))}`)
+            return v
+          })
+          .map(v =>
+            readFile(v)
+              .then(gest(schema, options))
+              .then(value => {
+                if (value.errors && value.data) return `${chalk.black.bgYellow(' WARNING ')} ${chalk.dim(v.replace(process.cwd(), '.'))}`
+                if (value.errors) return `${chalk.black.bgRed(' FAIL ')} ${chalk.dim(v.replace(process.cwd(), '.'))}`
+                return `${chalk.black.bgGreen(' PASS ')} ${chalk.dim(v.replace(process.cwd(), '.'))}`
+              })
+              .then(console.log)
+              .catch(console.log)
+          ))
+        .catch(console.log)
     } else {
-      // REPL
-      REPL(schema, options)
+      // DEFAULT COMMAND
+      if (args.sub && args.sub.length) {
+        checkPath(path.join(process.cwd(), args.sub[0]))
+        .then(readFile)
+        .catch(() => args.sub[0])
+        .then(gest(schema, options))
+        .then(colorResponse)
+        .then(console.log)
+        .catch(console.log)
+      } else {
+        // REPL
+        REPL(schema, options)
+      }
     }
   }
 } catch (e) {
--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const fs = require('fs')
+const path = require('path')
 
 exports.DEFAULT_CONFIG = { timeout: 10000, headers: {} }
 
@@ -58,3 +59,42 @@ or with \`schema.js\` in the current working directory.
     default: return e
   }
 }
+
+exports.findFiles = function (regex = /.*.(query|graphql)/i) {
+  const check = (name, file) => {
+    const newFile = path.join(name, file)
+    if (file === 'node_modules' || file === '.git') return
+    return new Promise((resolve, reject) => {
+      fs.stat(newFile, (err, stats) => {
+        if (err) return reject(err)
+        if (stats && stats.isDirectory()) return resolve(readDir(newFile))
+        if (regex.exec(newFile)) return resolve(newFile)
+        return resolve()
+      })
+    })
+  }
+
+  const readDir = (name) =>
+    new Promise((resolve, reject) =>
+      fs.readdir(name, (err, files) =>
+        err ? reject(err) : resolve(Promise.all(
+          files.map(check.bind(null, name))
+        )))).then(values =>
+          values.reduce((x, y) => x.concat(y), [])
+                .filter(value => value))
+
+  return readDir(process.cwd())
+}
+
+// return resolve(Promise.all(
+//   files.map(f => new Promise((resolve, reject) => {
+//     if (regex.exec(f)) {
+//       return resolve(f)
+//     }
+    // fs.stat(f, (err, stats) => {
+    //   if (err) return reject(err)
+    //   // if (stats && stats.isDirectory()) return resolve(readDir(f))
+    //   if (regex.exec(f)) {
+    //     return resolve(f)
+    //   }
+    // })
