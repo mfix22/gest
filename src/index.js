439f746ed9fbdf3b39d30254963beee918553d65
Mike Fix <mrfix84@gmail.com>
2017-03-28 13:30:14 -0500

    Update `gest` index to allow global function Primarily for jest testing

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ const axios = require('axios')
 const { graphql } = require('./import').getGraphQL()
 const { correctURL, encode } = require('./util')
 
-function Gest (schema, config = {}) {
-  const { baseURL, headers, timeout } = Object.assign({ timeout: 10000, headers: {} }, config) // default config
-  return function (query) {
+const ENV = process.env.NODE_ENV
+
+const DEFAULT = {
+  timeout: 10000,
+  headers: {},
+  globals: ENV === 'test' && (global.test !== undefined || global.it !== undefined),
+  verbose: ENV === 'dev' || ENV === 'development'
+}
+
+function Gest (schema, config) {
+  const { baseURL, headers, timeout, globals, verbose } = Object.assign(DEFAULT, config) // default config
+
+  const gest = (query) => {
     if (baseURL) {
       const instance = axios.create({
         timeout,
@@ -13,16 +23,31 @@ function Gest (schema, config = {}) {
       })
 
       const corrected = correctURL(baseURL)
-      if (config.verbose) console.log(`${query} -> ${corrected}`)
+      if (verbose) console.log(`${query} -> ${corrected}`)
 
       return instance.post(corrected, encode(query))
                      .then(res => res.data)
                      .catch(e => e.response.data)
     }
 
-    if (config.verbose) console.log(query)
+    if (verbose) console.log(query)
     return graphql(schema, query, null, { headers })
   }
+
+  if (globals) {
+    // test global gest() function for shortcut testing with `jest`
+    global['gest'] = (name, query) => {
+      const test = global.test || global.it
+      test(name, () => {
+        return gest(query).then(result => {
+          if (result.errors) throw result.errors
+          return result
+        })
+      })
+    }
+  }
+
+  return gest
 }
 
 module.exports = exports.default = Gest
