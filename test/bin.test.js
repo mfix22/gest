e18193e811816ab0d48d9e6679b8bb9a3f0616b0
Mike Fix <mrfix84@gmail.com>
2019-05-30 12:01:59 -0700

    update bin test

--- test/bin.test.js
+++ test/bin.test.js
@@ -5,10 +5,14 @@ function run(command) {
   return execa.stdout('node', [path.resolve(__dirname, '..', 'bin', 'gest'), command])
 }
 
-const TEST_CASES = ['--inspect', '-I', '{ test }', '{ _test }']
+test.each(['--inspect', '-I', '{ test }'])(`success cases %s`, async flag => {
+  expect(await run(flag)).toMatchSnapshot()
+})
 
-TEST_CASES.forEach(flag => {
-  test(`gest ${flag}`, async () => {
-    expect(await run(flag)).toMatchSnapshot()
-  })
+test.each(['{ _test }'])(`failures case: %s`, async flag => {
+  try {
+    await run(flag)
+  } catch (e) {
+    expect(e.stdout).toMatchSnapshot()
+  }
 })
